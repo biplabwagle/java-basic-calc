2997789a85e68b1ecca1cf2fc981585756e1e5d3
biplab-w <waglebip@gmail.com>
2023-11-26 23:03:26 -0600

    Modified Main class

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.add(3,4));
-        System.out.println(calc.multiply(4,1));
+        System.out.println(calc.subtract(4,1));
         System.out.println(calc.multiply(4,5));
 
 
